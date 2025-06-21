a24c1908b68be1f9252b5892eb79aec700982976
Frankieli123 <3180623@gmail.com>
2025-06-22 00:36:07 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -17,7 +17,7 @@ https://raw.githubusercontent.com/mfuu/v2ray/master/v2ray
 `;
 
 let urls = [];
-let subConverter = "https://sub.nasl.cc:8888/sub"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
+let subConverter = "SUBAPI.cmliussss.net"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 let subConfig = "https://raw.githubusercontent.com/liandu2024/clash/refs/heads/main/Clash-LIAN.ini"; //订阅配置文件
 let subProtocol = 'https';
 
